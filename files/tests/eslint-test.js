0aad3799fb3c75a7ae9899d773f655fcdd2ba586
Stefan Penner <stefan.penner@gmail.com>
2017-07-21 22:00:36 -0700

    switch to QUnit

--- files/tests/eslint-test.js
+++ files/tests/eslint-test.js
@@ -1,10 +1,3 @@
 'use strict';
 
-const lint = require('mocha-eslint');
-const path = require('path');
-
-lint([
-  path.join(__dirname, '../*.js'),
-  path.join(__dirname, '../lib/**/*.js'),
-  path.join(__dirname, '**/*-test.js'),
-]);
+require('qunit-eslint')(['*.js', 'lib/**/*.js', 'tests/**/*-test.js']);
